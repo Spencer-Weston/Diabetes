f5c4e9f8d0328f8e38b43cbea90c7f31c48c8f8b
Daniel Graves <dgravesa@gmail.com>
2018-06-02 19:27:34 -0400

    initialized form and data submission server

---
+++ form_demo/README.md
@@ -0,0 +1,22 @@
+
+## Form Demo
+
+To start the server:
+
+```
+node main.js
+```
+
+Currently, server hosts on port 3000.
+This may be changed in **main.js**.
+To access the site, go to [localhost:3000](localhost:3000).
+
+Currently, the form consists of a simple string field.
+The string once submitted is appended to a file **data_file.txt** located on the server in the same directory as the main script and HTML form code.
+
+## Next Steps
+
+* modify form data in HTML code to take useful information
+* include time component in submission (default to current time unless user specifies)
+* modify **main.js** to write updated form input to data file
+
--- form_demo/main.js
+++ form_demo/main.js
@@ -1,12 +1,45 @@
-const http = require('http')
-const fs = require('fs')
+const http = require('http');
+const fs = require('fs');
+const qs = require('querystring');
 
 http.createServer(function (req, res) {
-  fs.readFile('form.html', function(err, data) {
+  if (req.method === "GET") {
+    fs.readFile('form.html', function(err, data) {
+      res.writeHead(200, {'Content-Type': 'text/html'});
+      res.write(data);
+      res.end();
+      //////////////////res.end();
+      console.log('Sent form to client.');
+    });
+  }
+  
+  else if (req.method === "POST") {  
+    // receive data from post
+    var body = '';
+    req.on('data', function (data) {
+      body += data;
+
+      // exit if post is too large
+      if (body.length > 1e6) {
+        req.connection.destroy();
+      }
+    });
+
+    // add entry to data file
+    req.on('end', function () {
+      var post = qs.parse(body);
+      //console.log(post['string']);
+
+      // append string to data file
+      fs.appendFile('data_file.txt', post['string'] + '\n', function (err) {
+        if (err) throw err;
+        console.log('added \'' + post['string'] + '\' to data file');
+      });
+    });
+    //console.log(req.body.string);
     res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write(data);
-    res.end();
-    console.log('Sent form to client.');
-  });
+    res.end('Thank you for your response!');
+  }
+  //res.end();
 }).listen(3000);
 
